d57aefc6973b2b2f7eb49957b739eee2df03cb64
Tim Kroon <timkroon0703@gmail.com>
2019-03-01 15:18:24 +0100

    tim

--- djApp.java
+++ djApp.java
@@ -3,9 +3,16 @@ package djApp;
 public class djApp {
 	static public void main(String [] args) {
 		System.out.println("Let the music start");
+		
 		DJ d = new DJ();
 		d.plaatInLaden();
+		
 		Koptelefoon k = new Koptelefoon();
 		k.koptelefoonmusic();
+		
+		Zanger Jan = new Zanger();
+		Jan.zingen();
+		
+		
 	}
 }

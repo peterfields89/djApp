41936f701cdb933a6f5be491833eb88bab51f722
Peter van der Velde <peterjavait@gmail.com>
2019-03-01 15:40:25 +0100

    trancedj toegevoegd

--- DJ.java
+++ DJ.java
@@ -5,3 +5,11 @@ class DJ {
 		System.out.println("plaatje is ingeladen");
 	}
 }
+class TranceDj extends DJ {
+	void plaatInLaden() {
+		System.out.println("plaatje is ingeleden in TranceDj");
+	}
+	void TranceDraaien(){
+		System.out.println("Draait trance");
+	}
+}
